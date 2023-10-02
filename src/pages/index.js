8287e7dc6a81778ad13700b27b6f44096e0f7323
vickystickz <victor.ademoyero@milsat.tech>
2023-10-02 07:58:32 +0100

    updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,175 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import React, { useState, useEffect } from "react";
+import Image from "next/image";
+import ymLogo from "../assets/logo.png";
+import fullYmLogo from "../assets/YM.png";
+import ymFutaLogo from "../assets/ym_futa.png";
+import FutaLogo from "../assets/futa_logo.png";
 
-const inter = Inter({ subsets: ['latin'] })
+function CountdownTimer() {
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  const [title, setTitle] = useState("");
+  const [isRunning, setIsRunning] = useState(false);
 
-export default function Home() {
+  useEffect(() => {
+    let intervalId;
+
+    const totalSeconds = hours * 3600 + minutes * 60 + seconds;
+
+    if (isRunning && totalSeconds > 0) {
+      intervalId = setInterval(() => {
+        const newTotalSeconds = totalSeconds - 1;
+        setHours(Math.floor(newTotalSeconds / 3600));
+        setMinutes(Math.floor((newTotalSeconds % 3600) / 60));
+        setSeconds(newTotalSeconds % 60);
+      }, 1000);
+    } else if (totalSeconds === 0) {
+      setIsRunning(false);
+    }
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [isRunning, hours, minutes, seconds]);
+
+  const startTimer = () => {
+    setIsRunning(true);
+  };
+
+  const resetTimer = () => {
+    setHours(0);
+    setMinutes(0);
+    setSeconds(0);
+    setIsRunning(false);
+    setTitle("");
+  };
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <div className="h-screen grid grid-rows-12 space-y-12">
+      <div className="flex col-span-1">
+        <div className="flex w-[350px] flex-col gap-4 ">
+          <div className="flex items-center gap-2">
+            <input
+              type="number"
+              value={hours}
+              className="text-black border-2 border-black p-1 rounded-md w-[6em]"
+              onChange={(e) => setHours(parseInt(e.target.value))}
             />
-          </a>
+            <span>Hours</span>
+          </div>
+          <div className="flex items-center gap-2">
+            <input
+              type="number"
+              value={minutes}
+              className="text-black border-2 border-black p-1 rounded-md w-[6em]"
+              border-2
+              border-black
+              onChange={(e) => setMinutes(parseInt(e.target.value))}
+            />
+            <span>Minutes</span>
+          </div>
+          <div className="flex items-center gap-2">
+            <input
+              type="number"
+              value={seconds}
+              className="text-black border-2 border-black p-1 rounded-md w-[6em]"
+              onChange={(e) => setSeconds(parseInt(e.target.value))}
+            />
+            <span>Seconds</span>
+          </div>
+          <div className="flex items-center gap-2">
+            <input
+              type="text"
+              value={title}
+              className="text-black border-2 border-black p-1 rounded-md w-[6em]"
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            <span>Session</span>
+          </div>
+          <button
+            onClick={startTimer}
+            className="border-black max-w-[100px] font-medium  border-2 rounded-md p-2"
+          >
+            Start
+          </button>
+          <button
+            onClick={resetTimer}
+            className="border-black max-w-[100px] font-medium  border-2 rounded-md p-2"
+          >
+            Reset
+          </button>
+        </div>
+        <div className="flex flex-col w-[65%] gap-6 items-center mt-2">
+          <Image
+            src={fullYmLogo}
+            alt="YouthMappers logo"
+            width={600}
+            height={600}
+          />
+
+          <i className="text-3xl font-medium">
+            YouthMappers Open Mapping For Heat and Health Workshop
+          </i>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="w-full flex flex-col items-center gap-6 justify-center col-span-10">
+        <div className="mt-8 flex flex-col items-center">
+          <div className="flex items-center gap-4">
+            <span className="text-xl font-medium">Session:</span>
+            <h3 className="text-4xl font-semibold">{title}</h3>
+          </div>
+
+          <div className="flex gap-4 items-center text-5xl">
+            <h3 className="text-[5em] font-semibold">
+              {String(hours).padStart(2, "0")}
+            </h3>
+            <span>:</span>
+            <h3 className="text-[5em] font-semibold">
+              {" "}
+              {String(minutes).padStart(2, "0")}
+            </h3>
+            <span>:</span>
+            <h3 className="text-[5em] font-semibold">
+              {String(seconds).padStart(2, "0")}
+            </h3>
+          </div>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="col-span-1 grid grid-cols-12 w-[100vw] justify-end">
+        <div className="col-span-4 flex items-center justify-start pl-6">
+          <div>
+            <Image
+              src={FutaLogo}
+              alt="Futa logo"
+              width={120}
+              height={120}
+            />
+          </div>
+        </div>
+        <div className="col-span-4 flex  items-center justify-center">
+          <i className="text-lg font-medium">Hosted By: </i>
+          <Image
+            src={ymFutaLogo}
+            alt="YouthMappers logo"
+            width={300}
+            height={300}
+          />
+        </div>
+        <div className="col-span-4  w-full flex justify-end">
+          <div>
+            <Image
+              src={ymLogo}
+              alt="YouthMappers logo"
+              width={200}
+              height={200}
+            />
+          </div>
+        </div>
       </div>
-    </main>
-  )
+    </div>
+  );
 }
+
+export default CountdownTimer;
